567c15573f386945c1124b41e2a684dabb358343
Ulysses Grundler <grundler@cern.ch>
2013-11-20 17:29:25 +0100

    Taken from NTUHEP CVS repo

---
+++ inclusiveSecondaryVertexFinderTagInfos_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoBTag.SecondaryVertex.secondaryVertexTagInfos_cfi import *
+
+inclusiveSecondaryVertexFinderTagInfos = secondaryVertexTagInfos.clone()
+
+# use external SV collection made from IVF
+inclusiveSecondaryVertexFinderTagInfos.extSVCollection     = cms.InputTag('inclusiveMergedVertices')
+inclusiveSecondaryVertexFinderTagInfos.extSVDeltaRToJet    = cms.double(0.3)
+inclusiveSecondaryVertexFinderTagInfos.useExternalSV = cms.bool(True)
+inclusiveSecondaryVertexFinderTagInfos.vertexCuts.fracPV = 0.79 ## 4 out of 5 is discarded
+inclusiveSecondaryVertexFinderTagInfos.vertexCuts.distSig2dMin = 2.0
+
+
+
+# filtered IVF as used in some analyses
+
+inclusiveSecondaryVertexFinderTagInfosFiltered = secondaryVertexTagInfos.clone()
+
+inclusiveSecondaryVertexFinderTagInfosFiltered.extSVCollection     = cms.InputTag('bToCharmDecayVertexMerged')
+inclusiveSecondaryVertexFinderTagInfosFiltered.extSVDeltaRToJet    = cms.double(0.5)
+inclusiveSecondaryVertexFinderTagInfosFiltered.useExternalSV = cms.bool(True)
+inclusiveSecondaryVertexFinderTagInfosFiltered.vertexCuts.fracPV = 0.79 ## 4 out of 5 is discarded
+inclusiveSecondaryVertexFinderTagInfosFiltered.vertexCuts.distSig2dMin = 2.0
+
+
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp = secondaryVertexTagInfos.clone()
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp.extSVCollection     = cms.InputTag('bToCharmDecayVertexMerged')
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp.useExternalSV = cms.bool(True)
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp.vertexCuts.fracPV = 0.79 ## 4 out of 5 is discarded
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp.vertexCuts.distSig2dMin = 2.0
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp.extSVDeltaRToJet    = cms.double(0.8)
+inclusiveSecondaryVertexFinderTagInfosFilteredTemp.vertexCuts.maxDeltaRToJetAxis = cms.double(0.8)
